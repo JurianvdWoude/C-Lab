d1baac1a5d8b411c3ac0d140bb84bbd2cc48d921
Jurian <blazeye@fedora-3.home>
2026-09-21 10:29:00 +0200

    added in time measurements using the chrono library

--- 02_patterns/game_loop/main.cpp
+++ 02_patterns/game_loop/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <chrono>
+
+
+decltype(std::chrono::steady_clock::now()) getCurrentTime() {
+  return std::chrono::steady_clock::now();
+}
 
 void processInput() {
   std::cout << "processing input" << '\n';
 }
 
-void update() {
-  std::cout << "updating game" << '\n';
+void update(double elapsedTime) {
+  std::cout << "updating game, elapsed time: " << elapsedTime << " s" << '\n';
 }
 
 void render() {
@@ -13,10 +19,15 @@ void render() {
 }
 
 int main() {
+  auto lastTime = getCurrentTime();
   while (true) {
+    auto current = getCurrentTime();
+    std::chrono::duration<double> elapsedTime = current - lastTime;
     processInput();
-    update();
+    update(elapsedTime.count());
     render();
+
+    lastTime = current;
   }
   return 0;
 }
